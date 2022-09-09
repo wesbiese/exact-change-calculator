ed7cc3ab63ccb381334c9bb6169859e60272b393
Wes Biese <wes.biese@phreesia.com>
2022-09-08 20:25:17 -0500

    Added currency selector

--- Interview.Web/src/App.js
+++ Interview.Web/src/App.js
@@ -3,9 +3,12 @@ import { useState } from "react"
 // UI Components
 import Header from "./ui-components/Header"
 import Box from "@mui/material/Box"
+import CoinList from "./ui-components/CoinList"
+import CurrencySelect from "./ui-components/CurrencySelect"
 import DollarAmountInput from "./ui-components/DollarAmountInput"
 import ExactChangeList from "./ui-components/ExactChangeList"
 import Form from "./ui-components/Form"
+import MenuItem from "@mui/material/MenuItem"
 import NumCoinsField from "./ui-components/NumCoinsField"
 
 import calcMinCoins from "./calcMinCoins"
@@ -19,10 +22,13 @@ export default function App() {
 	// React state variable, useState() preserves value between renders, 20.22 is initial value
 	const [amount, setAmount] = useState(20.22)
 
-	// React hook to get 'coins' object on page load
-	const coins = useGetCoins()
+	const [currency, setCurrency] = useState("")
+	
+	// React hook to get 'coins' object on page load and as the currency selection changes
+	const coins = useGetCoins(currency)
 	const coinValues = coins.coinValues || []
 	const coinValueMap = coins.coinValueMap
+	const denominations = coins.denominations
 	const symbol = coins.symbol
 
 	// Calculate the minimum coins for a given lists of coin values and dollar amount
@@ -34,6 +40,12 @@ export default function App() {
 		<Box>
 			<Header>Exact Change Calculator</Header>
 			<Form>
+				<CurrencySelect value={currency} setValue={setCurrency}>
+					<MenuItem value="USD">USD ($)</MenuItem>
+					<MenuItem value="Euro">Euro (€)</MenuItem>
+					<MenuItem value="CAD">CAD ($)</MenuItem>
+				</CurrencySelect>
+				<CoinList label="Available Coins" denominations={denominations} />
 				<DollarAmountInput value={amount} setValue={setAmount} symbol={symbol} />
 				{coinCount &&
 					<NumCoinsField value={coinCount} />
--- Interview.Web/src/constants.js
+++ Interview.Web/src/constants.js
@@ -1,1 +1,26 @@
 export const API_URL = "http://localhost:5063/api"
+
+export const CURRENCIES = [{
+		currency: "USD",
+		symbol: "$",
+		denominations: [
+			{ label: "1¢", value: 1 },
+			{ label: "5¢", value: 5 },
+			{ label: "10¢", value: 10 },
+			{ label: "25¢", value: 25 }
+		]
+	}, {
+		currency: "Euro",
+		symbol: "€",
+		denominations: [
+			{ label: "1c", value: 1 },
+			{ label: "2c", value: 2 },
+			{ label: "5c", value: 5 },
+			{ label: "10c", value: 10 },
+			{ label: "20c", value: 20 },
+			{ label: "50c", value: 50 },
+			{ label: "1€", value: 100 },
+			{ label: "2€", value: 200 }
+		]
+	}
+]
